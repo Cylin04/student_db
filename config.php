0f98eb6cc957b076694703377546bd86d003ca23
Cylin04 <113584982+Cylin04@users.noreply.github.com>
2023-06-05 17:23:43 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+$dataHost = 'localhost';
+$dataName = 'student_db';
+$dataUserName = 'root';
+$dataPassWord = '';
+$mysqli = mysqli_connect($dataHost,$dataUserName,$dataPassWord,$dataName);
+?>
